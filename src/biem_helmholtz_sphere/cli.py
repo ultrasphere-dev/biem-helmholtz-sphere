5b337e786c3e1c74c26bf2f7e1b0ccc7dbb97741
34j <34j.github@proton.me>
2025-10-18 10:33:40 +0900

    feat(cli): add jascome

--- src/biem_helmholtz_sphere/cli.py
+++ src/biem_helmholtz_sphere/cli.py
@@ -1,8 +1,11 @@
+from logging import DEBUG, INFO, basicConfig, getLogger
 from pathlib import Path
 from typing import Any, Literal
 
 import typer
 from array_api._2024_12 import ArrayNamespaceFull
+from rich.logging import RichHandler
+from tqdm.rich import tqdm_rich
 from ultrasphere import create_from_branching_types
 
 from biem_helmholtz_sphere.bempp_cl_sphere import bempp_cl_sphere
@@ -11,6 +14,12 @@ from biem_helmholtz_sphere.biem import BIEMResultCalculator, biem, plane_wave
 from .gui import serve as serve_plot
 
 app = typer.Typer()
+LOG = getLogger(__name__)
+
+
+@app.callback()
+def _main(verbose: bool = typer.Option(False, "--verbose", "-v")) -> None:
+    basicConfig(handlers=[RichHandler(rich_tracebacks=True)], level=DEBUG if verbose else INFO)
 
 
 @app.command()
@@ -23,7 +32,7 @@ def serve() -> None:
 def jascome(
     backend: Literal["numpy", "torch"] = "numpy",
     device: str = "cpu",
-    dtype: str = "float32",
+    dtype: str = "float64",
     btanching_types: str = "a,ba,bpa,bba,bpbpa,caa",
 ) -> None:
     """Numerical examples for JASCOME."""
@@ -35,45 +44,51 @@ def jascome(
         from array_api_compat import torch as xp  # type: ignore
     with Path("jascome_output.csv").open("w") as f:
         f.write("branching_types,n_end,uscat_norm\n")
-        for btype in reversed(branchin_types):
-            try:
-                for n_end in range(10):
-                    c = create_from_branching_types(btype)
-                    calc: BIEMResultCalculator[Any, Any] = biem(
-                        c,
-                        uin=plane_wave(
-                            k=xp.asarray(1.0, device=device, dtype=dtype),
-                            direction=xp.asarray(
-                                (1,) + (0.0,) * (c.c_ndim - 1), device=device, dtype=dtype
-                            ),
-                        ),
+    for btype in tqdm_rich(list(reversed(branchin_types)), position=0):
+        try:
+            for n_end in tqdm_rich(list(range(1, 10)), position=1, leave=False):
+                c = create_from_branching_types(btype)
+                calc: BIEMResultCalculator[Any, Any] = biem(
+                    c,
+                    uin=plane_wave(
                         k=xp.asarray(1.0, device=device, dtype=dtype),
-                        n_end=n_end,
-                        eta=xp.asarray(1.0, device=device, dtype=dtype),
-                        centers=xp.asarray(
-                            (
-                                (
-                                    0.0,
-                                    2.0,
-                                )
-                                + (0.0,) * (c.c_ndim - 2),
-                                (
-                                    0.0,
-                                    -2.0,
-                                )
-                                + (0.0,) * (c.c_ndim - 2),
-                            ),
-                            device=device,
-                            dtype=dtype,
+                        direction=xp.asarray(
+                            (1,) + (0.0,) * (c.c_ndim - 1), device=device, dtype=dtype
                         ),
-                        radii=xp.asarray((1.0, 1.0), device=device, dtype=dtype),
-                        kind="outer",
+                    ),
+                    k=xp.asarray(1.0, device=device, dtype=dtype),
+                    n_end=n_end,
+                    eta=xp.asarray(1.0, device=device, dtype=dtype),
+                    centers=xp.asarray(
+                        (
+                            (
+                                0.0,
+                                2.0,
+                            )
+                            + (0.0,) * (c.c_ndim - 2),
+                            (
+                                0.0,
+                                -2.0,
+                            )
+                            + (0.0,) * (c.c_ndim - 2),
+                        ),
+                        device=device,
+                        dtype=dtype,
+                    ),
+                    radii=xp.asarray((1.0, 1.0), device=device, dtype=dtype),
+                    kind="outer",
+                )
+                uscat = calc.uscat(xp.asarray((0.0,) * c.c_ndim, device=device, dtype=dtype))
+                with Path("jascome_output.csv").open("a") as f:
+                    f.write(
+                        f"{btype},{n_end},{complex(uscat)},"
+                        f"{device},{dtype},"
+                        f"{calc.density.dtype},{calc.density.device},"  # type: ignore
+                        f"{uscat.dtype},{uscat.device}\n"
                     )
-                    uscat = calc.uscat(xp.asarray((0.0,) * c.c_ndim, device=device, dtype=dtype))
-                    f.write(f"{btype},{n_end},{complex(uscat)},{device},{dtype}\n")
-            except Exception as e:
-                print(e)
-                continue
+        except Exception as e:
+            LOG.error(e)
+            continue
 
 
 @app.command()
