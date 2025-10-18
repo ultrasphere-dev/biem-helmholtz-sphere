3d894884492e513c6d053c5cad80b7ac07254762
34j <34j.github@proton.me>
2025-10-18 11:23:13 +0900

    feat: add hpc

--- hpc/miyabi.sh
+++ hpc/miyabi.sh
@@ -5,8 +5,10 @@
 #PBS -m abe
 #PBS -l select=1
 
+module load cuda nvidia
+cd ${PBS_O_WORKDIR}
 cd biem-helmholtz-sphere
 git pull
-~/.local/bin/uv sync
-~/.local/bin/uv run biem-helmholtz-sphere jascome --backend=torch --device=cuda
-~/.local/bin/uv run biem-helmholtz-sphere jascome-bempp
+uv sync --all-extras
+uv run biem-helmholtz-sphere jascome --backend=torch --device=cuda
+uv run biem-helmholtz-sphere jascome-bempp
