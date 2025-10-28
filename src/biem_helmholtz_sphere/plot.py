ea1c470aaba5c87ff969db8a09fd44df51c84068
34j <34j.github@proton.me>
2025-10-28 16:07:15 +0900

    feat(gui): better gui

--- src/biem_helmholtz_sphere/plot.py
+++ src/biem_helmholtz_sphere/plot.py
@@ -2,10 +2,8 @@ from collections import defaultdict
 from collections.abc import Sequence
 from typing import Any
 
-import array_api_extra as xpx
 import plotly.express as px
 from array_api_compat import array_namespace
-from pandas import DataFrame
 from plotly.graph_objects import Figure
 
 from .biem import BIEMResultCalculator
@@ -79,35 +77,14 @@ def plot_biem(
     else:
         uin = biem_res.uin(cartesian)
     uscateach = biem_res.uscat(cartesian, per_ball=True)
-    uscat = xp.sum(uscateach, axis=-1)
-    n_spheres = int(uscateach.shape[-1])
 
     # time
     t = xp.arange(n_t)[:, None, None] / n_t
     texp = xp.exp(-1j * t * xp.asarray(2 * xp.pi))
-    shape = (n_t, *xpx.broadcast_shapes(x.shape, y.shape))
     uplot = plot_uin * uin + xp.sum(plot_uscateach_[None, None, :] * uscateach, axis=-1)
     uplot_re = xp.real(uplot * texp)
     if log:
         uplot_re = xp.sign(uplot_re) * xp.log1p(xp.abs(uplot_re))
-
-    df = DataFrame(
-        {
-            k: xp.reshape(v, (-1,))
-            for k, v in (
-                {
-                    "x": xp.broadcast_to(x[None, ...], shape),
-                    "y": xp.broadcast_to(y[None, ...], shape),
-                    "t": xp.broadcast_to(t, shape),
-                    "uin": xp.real(uin * texp),
-                    "uscat": xp.real(uscat * texp),
-                    "uall": xp.real((uin + uscat) * texp),
-                    "uplot": uplot_re,
-                }
-                | {f"uscat{i}": xp.real(uscateach[..., i] * texp) for i in range(n_spheres)}
-            ).items()
-        }
-    )
 
     # title
     title = ""
@@ -128,22 +105,19 @@ def plot_biem(
         f"η={float(biem_res.eta):g}"
     )
 
-    plot_2d = px.scatter(
-        df,
-        x="x",
-        y="y",
-        color="uplot",
-        range_color=[-df["uplot"].abs().max(), df["uplot"].abs().max()],
-        range_x=[xspace_[0], xspace_[1]],
-        range_y=[yspace_[0], yspace_[1]],
-        custom_data=["t", "uin", "uscat", "uall"] + [f"uscat{i}" for i in range(n_spheres)],
-        hover_data={"t": False, "uin": True, "uscat": True, "uall": True}
-        | {f"uscat{i}": True for i in range(n_spheres)},
-        animation_frame="t",
+    max_abs = xp.max(xp.abs(uplot_re))
+    plot_2d = px.imshow(
+        xp.moveaxis(uplot_re, -1, -2)[0, ...],
+        # animation_frame=0,
+        y=xp.squeeze(x),
+        x=xp.squeeze(y),
+        zmin=-max_abs,
+        zmax=max_abs,
+        range_color=[-max_abs, max_abs],
         title=title,
         labels={
-            "x": f"x{xaxis}",
-            "y": f"x{yaxis}",
+            "x": f"$x_{xaxis}$",
+            "y": f"$x_{yaxis}$",
         },
         color_continuous_scale="RdBu_r",
         **plot_kwargs,
